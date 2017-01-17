b2256a01df5d57ea049ec435918e9a1398d82d24
Alexander Tornoreanu <alex@tornoreanu.at>
2017-01-17 17:40:16 +0100

    first commit

---
+++ gameoflife.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    printf("Hello world!\n");
+
+
+    return EXIT_SUCCESS;
+}
+
+/**
+ * here some detailed comments :)
+ * 
+ * . nw . n . ne 
+ * .  w . c . e
+ * . sw . s . se
+ */
+int lifeFunction(int nw, int n, int ne, int w, int c, int e, int sw, int s, int se) {
+
+
+    return 0;
+}
